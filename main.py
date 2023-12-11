d9fdac81356e6a477a5e6a418064b2ae8c9cbe88
Brian Morava <zmorava515@hotmail.com>
2023-12-10 22:30:34 -0500

    removed unused import

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 from fastapi import Body, FastAPI
 from typing import Optional
 
