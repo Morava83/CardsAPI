81fa5722eb55153d5d594258a195674b3a2f0b0b
Brian Morava <zmorava515@hotmail.com>
2023-12-10 20:43:18 -0500

    decklist in error message

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import string
-from fastapi import FastAPI
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from fastapi import Body, FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -7,10 +13,50 @@ app = FastAPI()
 async def root():
     return {"PokemonCardsAPI": "Welcome!"}
 
-@app.get('/Decklist')
-async def decklist():
+@app.get('/Deck')
+async def deckListBase():
     return {"Enter decklist as a query string"}
 
-@app.get('/Decklist/{decklist_id}')
-async def decklist(decklist_id: str):
-    return {"decklist": decklist_id}
+
+@app.post('/Deck')
+async def getDeckList(decklist: Optional[str] = Body(None)):
+    # Process the decklist here (e.g., store it in a database, analyze it, etc.)
+    return {"image urls": decklist}
+
+    # Setup webdriver
+    webdriver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=webdriver_service)
+    
+    # Get webpage
+    driver.get('https://limitlesstcg.com/tools/imggen')
+
+    # Locate and interact with the text area using the name attribute
+    text_area = driver.find_element(By.NAME, 'input')
+    text_area.clear()
+
+    # Input the decklist
+    text_area.send_keys(decklist)
+
+    # Locate and click the "Submit" button
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit' and text()='Submit']")
+    submit_button.click()
+
+    # Wait for a moment to ensure the submission is processed
+    time.sleep(2)
+
+    # Get all images
+    images = driver.find_elements(By.TAG_NAME, 'img')
+
+    # Extract URLs
+    image_urls = [img.get_attribute('src') for img in images]
+
+    # Create a subset of image_urls (cards_urls) with "XS.png" in their name
+    cards_urls = [url for url in image_urls if 'XS.png' in url]
+
+    #Make the card images larger
+    cards_urls = [url.replace('XS.png', 'LG.png') for url in cards_urls]
+
+    # Close the browser
+    driver.quit()
+
+    #return {"image urls": cards_urls}
--- sample.py
+++ sample.py
@@ -1,5 +1,19 @@
 import requests
 
-request = requests.get("http://127.0.0.1:8000/")
+home = requests.get("http://127.0.0.1:8000/")
 
-print(request.json())
+print(home.json())
+
+
+base = requests.get("http://127.0.0.1:8000/Deck")
+
+print(base.json())
+
+url = "http://127.0.0.1:8000/Deck"
+decklist = "hi"  # Your single-line string
+
+payload = {"decklist": decklist}
+headers = {"Content-Type": "application/json"}
+
+response = requests.post(url, json=payload, headers=headers)
+print(response.json())
