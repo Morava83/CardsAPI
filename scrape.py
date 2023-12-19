c5a382557580156c205cee87e93c340d32abc7a6
Brian Morava <zmorava515@hotmail.com>
2023-12-19 01:40:09 -0500

    scrape being stable

--- scrape.py
+++ scrape.py
@@ -22,7 +22,36 @@ driver.get('https://limitlesstcg.com/tools/imggen')
 root = tk.Tk()
 root.withdraw()  # Hide the main window
 
-decklist = simpledialog.askstring("Input", "Paste your decklist here:")
+decklist = """
+3 Roaring Moon ex PAR 124
+1 Squawkabilly ex PAL 169
+1 Galarian Moltres V CRE 97
+1 Brute Bonnet PAR 123
+1 Radiant Greninja ASR 46
+1 Morpeko PAR 121
+1 Lumineon V BRS 40
+
+4 Professor Sada's Vitality PAR 170
+1 Iono PAL 185
+1 Boss's Orders PAL 172
+4 Battle VIP Pass FST 225
+4 Dark Patch ASR 139
+4 Cross Switcher FST 230
+4 Earthen Vessel PAR 163
+4 Energy Switch SVI 173
+3 Ultra Ball SVI 196
+2 Switch Cart ASR 154
+1 Hisuian Heavy Ball ASR 146
+1 Super Rod PAL 188
+1 Pal Pad SVI 182
+1 Canceling Cologne ASR 136
+2 Ancient Booster Energy Capsule PAR 159
+3 PokéStop PGO 68
+1 Collapsed Stadium BRS 137
+
+7 Darkness Energy 7
+3 Water Energy 3
+""" 
 
 # Locate and interact with the text area using the name attribute
 text_area = driver.find_element(By.NAME, 'input')
